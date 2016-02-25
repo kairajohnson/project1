7a7047293d212a71e7e3f4cfa095959acdc850ad
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-02-25 13:56:51 -0500

    updated at 1:56pm

--- kairajohnson_project-01.js
+++ kairajohnson_project-01.js
@@ -18,12 +18,6 @@ document.getElementById("welcome_text").appendChild(welcome_header);
 var gameStart=document.getElementById('letsplay')
 
 
-function changeBackgroundImageQuestion1 (){
-  document.body.style.backgroundImage = "url('cycling.jpg')";
-}
-
-gameStart.addEventListener('click', changeBackgroundImageQuestion1);
-
 var welcomebutton = document.getElementById('letsplay')
 welcomebutton.addEventListener('click',hideshow,false);
 
@@ -32,15 +26,20 @@ function hideshow() {
   this.style.display = 'none'
   document.getElementById('welcome_text').style.display = 'block';
   welcome_text.style.display ='none'
-  document.getElementById("trivia_box").style.display = 'block'
+  document.getElementById("wrapper").style.display = 'block'
   loopquestion()
 }
 //Questions Loop
 var trivia_questions =[
   "Your going to a gig a mile away, are you riding a bike?", "Your drinking a soda while drving. When you finish, would you throw it out the window?", "You just got an awesome mason jar with your jam, when it's empty, will you reuse it?", "Styrofoam plates, cups, etc. are bad for the earth, will you still buy it?"," Do you put recycables in the recyling bin?"
 ];
+
+var bg_images =["cycling.jpg", "soda.jpg", "Peanut_butter.jpg", "birthday.jpg", "recyclebin.jpg"]
+
 var points = 0;
-var input=document.querySelectorAll(".button")
+var input=document.querySelectorAll("#button")
+var yes = document.querySelector(".yes")
+var pictureIndex=0;
 var index = 0;
 
 function loopquestion () {
@@ -53,12 +52,30 @@ function loopquestion () {
   }
   document.getElementById("questionBox").innerHTML =trivia_questions[index++];
 
-
-
-  if(input === "yes"){
-    points = points + 1;
-
-  }
+if (pictureIndex<5){
+  document.body.style.backgroundImage="url(" + bg_images[pictureIndex++] + ")"
+}
 }
 
-//alert("You got " + points + " points!");
+
+
+//score
+
+
+function yesButton (){
+    console.log(this)
+  if(this === yes){
+    points = points + 1;
+  document.getElementById('score').innerHTML = "SCORE: " + points;
+}
+}
+function yesButton (){
+    console.log(this)
+  if(this === yes){
+    points = points + 1;
+  document.getElementById('score').innerHTML = "SCORE: " + points;
+}
+}
+yes.addEventListener('click', yesButton)
+
+document.getElementById('score').innerHTML = "SCORE: " + points;
